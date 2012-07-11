9a6f32f91f0208ef98987a9b854e1ba29f8ad53f
srsudo <srsudo@769aabb4-c2dc-11de-b39d-8dab272b6a2a>
2012-07-11 18:19:32 +0000

    cambios

--- vendor/couchapp/_attachments/jquery.couchLogin.js
+++ vendor/couchapp/_attachments/jquery.couchLogin.js
@@ -30,6 +30,7 @@
                         elem.html(templates.loggedOut);
                         if (opts.loggedOut) {opts.loggedOut()}
                         $("#account").css({'float':'none'});
+                        $("#etiqueta").hide();
                     };
                 }
             });
@@ -70,7 +71,8 @@
     var templates = {
         adminParty : '<p><strong>Admin party, everyone is admin!</strong> Fix this in <a href="/_utils/index.html">Futon</a> before proceeding.</p>',
         
-        loggedOut : '<div id="marco1" class="estilo_marco"> <img id="imagenmapa" src="Images/concierto.jpg"></div> <div id="marco2"> <form class="login" class="marco_index"><fieldset>	<legend>Log in</legend>	<input class="estilo_input text_input" type="text" autocorrect="off" autocapitalize="off" placeholder="Nombre" value="" name="name"><br>	<input class="estilo_input text_input" type="password" placeholder="contraseña" value="" name="password"><br>	<input  class="estilo_boton text_boton" title="ok" type="submit" value="Login">	</fieldset></form>	<form class="signup" class="marco_index"><fieldset><legend>Sign up</legend>	<input class="estilo_input text_input" type="text" autocorrect="off" autocapitalize="off" placeholder="Introduce un nombre" value="" name="name"><br>	<input class="estilo_input text_input" type="password" placeholder="Password" value="" name="password"><br><input  class="estilo_boton text_boton" title="ok" type="submit" value="Signup"><br></fieldset></form></div>',
+        loggedOut : 
+'<span id="info"></span><div id="marco1" class="estilo_marco"> <img id="imagenmapa" src="Images/concierto.jpg"></div> <div id="marco2"> <form class="login" class="marco_index"><fieldset>	<legend>Log in</legend>	<input class="estilo_input text_input" type="text" autocorrect="off" autocapitalize="off" placeholder="Nombre" value="" name="name"><br>	<input class="estilo_input text_input" type="password" placeholder="contraseña" value="" name="password"><br>	<input  class="estilo_boton text_boton" title="ok" type="submit" value="Login">	</fieldset></form>	<form class="signup" class="marco_index"><fieldset><legend>Sign up</legend>	<input class="estilo_input text_input" type="text" autocorrect="off" autocapitalize="off" placeholder="Introduce un nombre" value="" name="name"><br>	<input class="estilo_input text_input" type="password" placeholder="Password" value="" name="password"><br><input  class="estilo_boton text_boton" title="ok" type="submit" value="Signup"><br></fieldset></form></div>',
         
         loginForm : '<form class="login"><label for="name">Name</label> <input type="text" name="name" value="" autocapitalize="off" autocorrect="off"><label for="password">Password</label> <input type="password" name="password" value=""><input type="submit" value="Login"><a href="#signup">or Signup</a></form>',
         
@@ -81,15 +83,15 @@
 /*      $("#info").html('<div id="account"><span>' + uri_name + ' </a>| <a href="#logout">Logout?</a></span></div>');*/
         var auth_db = encodeURIComponent(r.info.authentication_db)
         , uri_name = encodeURIComponent(r.userCtx.name)
-        , span = $('<span>' + uri_name + ' </a>| <a href="#logout">Logout?</a></span>');
+        , span = $('<span id="usuarios">' + uri_name + ' </a>| <a href="#logout">Logout?</a></span>');
         $('a.name', span).text(r.userCtx.name); // you can get the user name here
-      
-       
+
 	$.ajax({
         	url:'seleccion.html', global:false, success: function(data){
         		$("#profile").html(data);
         	}        
         });
+	$("#etiqueta").show();
         $("#account").css({'float':'right', 'position':'relative'});
         $("#profile").show();
         
